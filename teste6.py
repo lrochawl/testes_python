251a44008bafa27076794f1fda6c6e21a7af4a44
Lucas Rocha <wltopos@gmail.com>
2023-04-23 19:05:41 -0300

    Update teste6.py

--- teste6.py
+++ teste6.py
@@ -1,2 +1,3 @@
-nome  = input('Qual é o seu nome? ') #função para coletar dados do usuario
-print(f'Seu nome é {nome}')
+numero_1  = int(input('Digite um numero: ')) #função para coletar dados do usuario
+numero_2  = int(input('Digite outro numero: ')) #função para coletar dados do usuario
+print(f'Seu nome é {numero_1 + numero_210}')
