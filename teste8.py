1658fc0377beb2546f1de6e2aadb2ca446ad280f
Lucas Rocha <wltopos@gmail.com>
2023-04-23 20:09:35 -0300

    Create teste8.py

---
+++ teste8.py
@@ -0,0 +1,16 @@
+#operadores relacionais ou de comparação 
+# 2 > 2 (falso) maior que
+# 2 >=2 (verdadeiro) maior ou igual
+# 2 < 2 (falso) menor que
+# 2 <= 2 (verdadeiro) menor ou igual
+# 2 == 2 (verdadeiro) igual a
+# 2 != 2 (false) diferente de
+
+maior       = 2 > 2 
+maior_igual = 2 >=2
+menor       = 2 < 2
+menor_igual = 2 <= 2
+igual       = 2 == 2
+diferente   = 2 != 2 
+
+print(diferente)
