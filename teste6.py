9a482f144aaabffa181ed006907d25f48f6c1d30
Lucas Rocha <wltopos@gmail.com>
2023-04-23 19:08:48 -0300

    Update teste6.py

--- teste6.py
+++ teste6.py
@@ -1,3 +1,7 @@
-numero_1  = int(input('Digite um numero: ')) #função para coletar dados do usuario
-numero_2  = int(input('Digite outro numero: ')) #função para coletar dados do usuario
-print(f'Seu nome é {numero_1 + numero_210}')
+numero_1  = input('Digite um numero: ') #função para coletar dados do usuario
+numero_2  = input('Digite outro numero: ') #função para coletar dados do usuario
+
+int_numero_1 = int(numero_1)
+int_numero_2 = int(numero_2)
+
+print(f'A soma dos numeros é {int_numero_1 + int_numero_2}')
