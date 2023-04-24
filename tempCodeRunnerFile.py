bb463d61012d4f778534b46fa0bd956e174360d8
Lucas Rocha <wltopos@gmail.com>
2023-04-24 10:50:04 -0300

    update
    #Operadores logiscos
    #and (e) or (ou) not (não)
    #and -  Todas as codições precisam ser verdadeiras
    #Se qualquer valor for considerado for falso toda a expressão será considerada falsa
    #também existe o tipo None que é utilizado para representar um não valor
    #qualquer valor que for considerado como verdadeiro toda a expressão será considerada como verdadeira

---
+++ tempCodeRunnerFile.py
@@ -0,0 +1,2 @@
+print(True and True and False) #False
+print((True or True) and False) #False
--- teste10.py
+++ teste10.py
@@ -3,6 +3,7 @@
 #and -  Todas as codições precisam ser verdadeiras
 #Se qualquer valor for considerado for falso toda a expressão será considerada falsa
 #também existe o tipo None que é utilizado para representar um não valor
+#qualquer valor que for considerado como verdadeiro toda a expressão será considerada como verdadeira
 
 entrada = input('[E] Entrar e [S] Sair: ')
 senha_digitada = input('Digite sua senha: ')
@@ -10,7 +11,7 @@ senha_permitida = '1234'
 
 if entrada == 'E' and senha_digitada == senha_permitida:
         print('Entrou')
-elif(entrada == 'E' and senha_digitada != senha_permitida):
+elif((entrada == 'E' or entrada == 'e') and senha_digitada != senha_permitida):
     print('Senha incorreta ')
 else:
     print('Saiu')
@@ -18,3 +19,6 @@ else:
 print(bool(0)) #False
 print(bool(0.0)) #False
 print(bool(False)) #False
+
+print(True and True and False) #False
+print((True or True) and False) #False
