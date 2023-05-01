a66fd2f171806facc71c4c7b5263b34ed50205a1
Lucas Rocha <wltopos@gmail.com>
2023-05-01 15:11:25 -0300

    Update teste14.py

--- teste14.py
+++ teste14.py
@@ -1,3 +1,11 @@
 #fatiamento de string
+#[inicio:fim:passo] 
+#em python geralmente o indice final não é incluido
+# se o passo for -1 inverte a string
+#len conta a quantidade de caracters
 
 variavel = "Olá mundo"
+
+print(len(variavel)) #mostra o tamanho da atring
+print(variavel[0:len(variavel):2]) # mostra a string com passo de dois em dois caracters
+print(variavel[::-1]) #inverte a string
